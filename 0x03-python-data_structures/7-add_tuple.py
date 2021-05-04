8dd0604ffde4725cc02e470fb4052a0aaf00383a
murageB <brendanrichard96@gmail.com>
2021-05-04 16:57:29 +0000

    adds two tuple

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    if len(tuple_a) < 2:
+        if len(tuple_a) == 0:
+            tuple_a = (0, 0)
+        else:
+            tuple_a = (tuple_a[0], 0)
+    if len(tuple_b) < 2:
+        if len(tuple_b) == 0:
+            tuple_b = (0, 0)
+        else:
+            tuple_b = (tuple_b[0], 0)
+
+    new_tuple = tuple(map(sum, zip(tuple_a, tuple_b)))
+    return(str(new_tuple))
