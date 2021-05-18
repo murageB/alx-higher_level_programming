0d403a69746c76500d9d23360ccef7d6032ce5bb
murageB <brendanrichard96@gmail.com>
2021-05-18 13:02:20 +0000

    define size attribute of the square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+class Square:
+
+    """
+    Square class defined by geometric shape
+
+    Attributes:
+          size (int): size of square
+    """
+
+    def __init__(self, size):
+        """ 
+        Initialize method
+
+        Args:
+            size (int): size of square
+        """
+
+        self.__size = size
