3dde5c3bad1c2ed76074e0812b33d935e1ff245f
murageB <brendanrichard96@gmail.com>
2021-05-22 15:51:06 +0000

    prints a text with 2 new lines after each of these characters: ., ? and :

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -8,7 +8,13 @@ Text Indentation module
 
 def text_indentation(text):
     """
+    prints a text with 2 new lines after 
+    each of these characters: ., ? and :
 
+    Args:
+       text (str): text to be split 
+    Raise:
+        TypeError: when text is not str
     """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
