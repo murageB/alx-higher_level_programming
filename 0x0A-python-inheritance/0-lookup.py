57a2fd7a192a189c3b4f0deab361831bfb8574ff
murageB <brendanrichard96@gmail.com>
2021-05-31 12:19:35 +0000

    look up ^

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Lookup function module
+
+"""
+
+def lookup(obj):
+    """
+    returns the list of available attributes and methods of an object
+
+    """
+    return dir(obj)
