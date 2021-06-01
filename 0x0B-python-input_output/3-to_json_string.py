47fee489c691cc2c1c7f840aee2aff11406ab275
murageB <brendanrichard96@gmail.com>
2021-06-01 08:03:26 +0000

    obj tp json string

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Object to JSON string Module
+
+"""
+
+'''Import json Module'''
+import json
+
+
+def to_json_string(my_obj):
+    """
+    returns the JSON representation of an object (string)
+
+    """
+    return json.dumps(my_obj)
