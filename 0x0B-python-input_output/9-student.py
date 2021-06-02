93fb20fb84f9e1f80271cfc5772783f89498dbc8
murageB <brendanrichard96@gmail.com>
2021-06-02 13:58:01 +0000

    class Student

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Student module
+
+"""
+
+
+class Student:
+    """
+    define a student
+    Attributes:
+        first_name
+        last_name
+        age
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        initialization method
+
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        retrieves a dictionary representation of a Student instance
+        """
+        return self.__dict__
